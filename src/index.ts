f4039b6c49e80516e097ee17d7a54d61e537b2aa
aboyce <adam@aboyce.uk>
2020-07-04 18:03:38 +0100

    Added required types.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,91 @@
+// Libraries
+import type {
+  GridTemplateColumnsProperty,
+  GridTemplateRowsProperty,
+  GridTemplateAreasProperty,
+  GridTemplateProperty,
+  GridGapProperty,
+  GridColumnGapProperty,
+  GridRowGapProperty,
+  JustifyItemsProperty,
+} from 'csstype'
+
 type DivProps = React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>
+
+type DisplayCSS = {
+  // 'display'
+  inline?: boolean
+}
+
+type GridTemplateColumnsCSS = {
+  // 'grid-template-columns'
+  columns?: GridTemplateColumnsProperty<string | 0>
+}
+
+type GridTemplateRowsCSS = {
+  // 'grid-template-rows'
+  rows?: GridTemplateRowsProperty<string | 0>
+}
+
+type GridTemplateAreasCSS = {
+  // 'grid-template-areas'
+  areas?: GridTemplateAreasProperty
+}
+
+type GridTemplateCSS = {
+  // 'grid-template'
+  template?: GridTemplateProperty
+}
+
+type GridGapCSS =
+  | {
+      // 'grid-gap' or 'gap'
+      gap?: GridGapProperty<string | 0>
+      gridGap?: never
+    }
+  | {
+      // 'grid-gap' or 'gap'
+      gap?: never
+      gridGap?: GridGapProperty<string | 0>
+    }
+
+type GridColumnGapCSS = {
+  // 'column-gap'
+  columnGap?: GridColumnGapProperty<string | 0>
+}
+
+type GridRowGapCSS = {
+  // 'row-gap'
+  rowGap?: GridRowGapProperty<string | 0>
+}
+
+type JustifyItemsCSS =
+  | {
+      // 'justify-items' short
+      justifyItemsStart?: boolean
+      justifyItemsEnd?: boolean
+      justifyItemsCenter?: boolean
+      justifyItemsStretch?: boolean
+      justifyItems?: never
+    }
+  | {
+      // 'justify-items' manual
+      justifyItemsStart?: never
+      justifyItemsEnd?: never
+      justifyItemsCenter?: never
+      justifyItemsStretch?: never
+      justifyItems?: JustifyItemsProperty
+    }
+
+export type GridContainerProps = DivProps &
+  DisplayCSS &
+  GridTemplateColumnsCSS &
+  GridTemplateRowsCSS &
+  GridTemplateAreasCSS &
+  GridTemplateCSS &
+  GridGapCSS &
+  GridColumnGapCSS &
+  GridRowGapCSS &
+  JustifyItemsCSS
+
+export type GridItemProps = DivProps
