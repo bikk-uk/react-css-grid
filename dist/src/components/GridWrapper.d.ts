ae3e9a74c86e20c1bff2cacb117aa90968df14a1
aboyce <adam@aboyce.uk>
2020-10-10 15:55:18 +0100

    Removed dist folder from repository.

--- dist/src/components/GridWrapper.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import type { GridContainerProps } from '../index'
-import GridItem from './GridItem'
-export declare class Grid extends React.Component<GridContainerProps> {
-  static Item: typeof GridItem
-  render(): React.ReactElement
-}
-export default Grid
